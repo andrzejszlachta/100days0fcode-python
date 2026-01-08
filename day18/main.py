ef0c072d6695c5f6449d3d6450b69d6eb4169331
Andrzej Szlachta <andrzej.sz123@gmail.com>
2026-01-08 23:56:25 +0100

    Day 17 - Exercises

---
+++ day18/main.py
@@ -0,0 +1,60 @@
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# tom = Turtle()
+# terry = Turtle()
+#
+# screen = Screen()
+#
+# # timmy_the_turtle.shape("turtle")
+# # timmy_the_turtle.color("red")
+#
+# for _ in range(0,4):
+#     tim.forward(100)
+#     tim.right(90)
+#
+# screen.exitonclick()
+import random
+
+import heroes
+import villains
+
+from turtle import Turtle, Screen
+
+# dash = Turtle()
+# dash.shape("arrow")
+#
+#
+# for _ in range(15):
+#     dash.forward(10)
+#     dash.penup()
+#     dash.forward(10)
+#     dash.pendown()
+
+# bob = Turtle()
+# screen = Screen()
+colors  = ["red","green","blue","orange","purple","pink","yellow"]
+#
+# for seq in range(3, 10):
+#     angle = int(360/seq)
+#     for _ in range(seq):
+#         bob.forward(50)
+#         bob.right(angle)
+#     color = random.choice(colors)
+#     bob.color(color)
+#
+# screen.exitonclick()
+
+bob = Turtle()
+bob.speed('fastest')
+bob.pensize(10)
+screen = Screen()
+
+turns = [0, 90, 180, 270]
+for _ in range(100):
+    bob.color(random.choice(colors))
+    direction = random.choice(turns)
+    bob.setheading(direction)
+    bob.forward(50)
+
+screen.exitonclick()
