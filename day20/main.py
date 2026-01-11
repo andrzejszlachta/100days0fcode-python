dc73192f2e0769b942adc0e1e332223b4c71c2ce
Andrzej Szlachta <andrzej.sz123@gmail.com>
2026-01-11 02:14:27 +0100

    Day 20 - Snake part 1

---
+++ day20/main.py
@@ -0,0 +1,32 @@
+import time
+from turtle import Screen, Turtle
+
+from day20.snake import Snake
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.up, "w")
+screen.onkey(snake.down, "s")
+screen.onkey(snake.left, "a")
+screen.onkey(snake.right, "d")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+
+screen.exitonclick()
